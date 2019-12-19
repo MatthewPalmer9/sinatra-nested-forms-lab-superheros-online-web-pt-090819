66020fb0f93b10c8b9906974cdf3a2e4872095f2
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-19 23:07:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      erb :super_hero 
+      erb :super_hero
+    end
+
+    post '/teams' do
+      
+      erb :team
     end
 
 
