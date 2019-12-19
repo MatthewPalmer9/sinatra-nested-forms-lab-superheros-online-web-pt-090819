8260cff7d64a1edd08096925a7652e7b3cf51d48
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-19 23:10:45 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,9 +1,19 @@
 class SuperHero
   attr_accessor :name, :power, :bio
 
+  @@all = []
+
   def initialize(params)
     @name = params[:name]
     @power = params[:power]
     @bio = params[:bio]
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
