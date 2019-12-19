4120d7cdab9a6b7458aabcb7c101932b99c21aca
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-19 23:14:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
       @team = Team.new(params[:team])
 
       params[:team][:heroes].each do |hero_info|
-        
+        SuperHero.new(hero_info)
       erb :team
     end
 
